400eac3ec40051f04e2e4df257484efb1fc4f53f
Mateusz Kuźnik <mateusz.kuznik@azimo.com>
2020-08-19 14:04:44 +0200

    added

---
+++ FindUsageInFrameworks/README.md
@@ -0,0 +1,7 @@
+### The script will try to find usage of a given argument in all frameworks located in a current folder recursively.
+
+For example, if you want to find all frameworks that are using 'UIWebView':
+
+```
+sh fundUsageInFrameworks.sh UIWebView
+```
---
+++ FindUsageInFrameworks/fundUsageInFrameworks.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# The script will try to find usage of a given argument in all frameworks located in a current folder recursively.
+# sh 
+
+SEARCH_VALUE=$1
+
+if [ $# -eq 0 ]; then
+      echo "No arguments supplied. You have to provide one argument."
+      exit 1
+fi
+
+LIST_OF_FOUNDED_FRAMEWORKS=""
+for FRAMEWORK_PATH in $(find . -iname "*.framework"); do
+  FRAMEWORK_NAME=$(basename $FRAMEWORK_PATH .framework)
+  RESULT=$(nm $FRAMEWORK_PATH/$FRAMEWORK_NAME | grep $SEARCH_VALUE)
+  if [ ! -z "$RESULT" -a "$RESULT" != " " ]; then
+        echo $FRAMEWORK_PATH/$FRAMEWORK_NAME
+        echo $RESULT
+        LIST_OF_FOUNDED_FRAMEWORKS="${LIST_OF_FOUNDED_FRAMEWORKS}\n${FRAMEWORK_NAME}"
+  fi
+done
+
+echo ""
+echo "$LIST_OF_FOUNDED_FRAMEWORKS"
